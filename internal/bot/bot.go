df66d39bc778aa88062120b8b666155755dfca0e
Thecone27 <ronnoc752@gmail.com>
2020-07-14 18:12:34 +0100

    Add flags for add access

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -132,6 +132,21 @@ func (b *bot) messageHandler() func(s *discordgo.Session, m *discordgo.MessageCr
 			access = append(access, m.Author.ID)
 
 			resp = b.createPrivateList(guild, list, access)
+		case "addtoPrivate", "ap":
+			var access []string
+			if len(m.MentionRoles) != 0 {
+				for _, r := range m.MentionRoles {
+					access = append(access, r)
+				}
+			}
+
+			if len(m.Mentions) != 0 {
+				for _, u := range m.Mentions {
+					access = append(access, u.ID)
+				}
+			}
+
+			resp = b.addAccessToList(guild, list, access, user, roles)
 		case "random", "rv":
 			resp = b.randomFromList(guild, list, user, roles)
 		case "remove", "r":
